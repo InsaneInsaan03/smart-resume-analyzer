c02e7818d16dd40a95c7abde1b6d0075cdab6262
Mohammad Insaan <insaneinsaan03@gmail.com>
2024-11-29 21:18:58 +0530

    Initial commit

---
+++ Smart_Resume_Analyser_App-master/android/MainActivity.java
@@ -0,0 +1,40 @@
+package com.insaneinsaan03.resumeanalyzer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Create WebView
+        webView = new WebView(this);
+        setContentView(webView);
+        
+        // Enable JavaScript
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        
+        // Configure WebView
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Load the Streamlit app URL (replace with your hosted app URL)
+        webView.loadUrl("https://your-streamlit-app-url.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
